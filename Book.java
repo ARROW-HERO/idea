c90c10078c63b442b444559eb91005b5911adc6f
The green arrow <1471629910@qq.com>
2019-11-10 21:37:40 +0800

    two idea

--- Book.java
+++ Book.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.apple.six;
 
 public class Book {
@@ -35,3 +36,35 @@ public class Book {
         this.pages = pages;
     }
 }
+=======
+package com.apple.chaptersix;
+
+public class Book {
+    int pages;
+
+    double price;
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    public void Book(int pages, double price) {
+        this.pages = pages;
+        this.price = price;
+    }
+            }
+>>>>>>> first idea
